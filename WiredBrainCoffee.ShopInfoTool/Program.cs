5ddad6b49ffbea9ddc2394b3bc353624ae1fdc47
comebellalacitta <comebellalacitta@hotmail.com>
2020-07-25 23:49:50 +0200

    Added Quit command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,13 +9,20 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Write 'help' to list available coffe shop commands");
+            Console.WriteLine("Write 'help' to list available coffe shop commands, " + 
+                "write 'quit' to exit the application");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
             while (true)
             {
                 var line = Console.ReadLine();
+
+                if(string.Equals("quit",line,StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
                 if (string.Equals("help",line,StringComparison.OrdinalIgnoreCase))
