87d080c934ebb08daaee6fbfa1ad2882590949ae
comebellalacitta <comebellalacitta@hotmail.com>
2020-07-25 22:12:55 +0200

    Implement help command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
-            //nga comebellalacitta
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help",line,StringComparison.OrdinalIgnoreCase))
+                {
+                    foreach(var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> {coffeeShop.Location}");
+                    }
+                }
+            }
+
+        }
     }
-  }
 }
